aa1d07894c23ba47cd223fd596a49d4e62300b95
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 17:29:36 +0530

    Create day18 que1.c

---
+++ day18 que1.c
@@ -0,0 +1,16 @@
+# Write a program to print all factors of a given number.
+
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+
+    printf("Factors of %d are: ", num);
+    for (int i = 1; i <= num; i++) {
+        if (num % i == 0)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
